9bc5808b7c867acef87a24f7021f4a41b53e0be9
wo3kie <wo3kie@hotmail.com>
2015-12-01 22:56:15 +0000

    Args

---
+++ Args.cs
@@ -0,0 +1,12 @@
+
+public class Args
+{
+    public static int Main(string [] args ){
+        foreach( string arg in args ){
+            System.Console.WriteLine( arg );
+        }
+
+        return 0;
+    }
+}
+
